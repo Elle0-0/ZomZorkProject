87e0092f46f351341467fe1cc0da5bb95969a30e
Elle0-0 <andreamichellevaz@gmail.com>
2024-05-04 19:25:02 +0100

    pls work

---
+++ ZomZork/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_ZomZork_FILE C:/Users/andre/OneDrive/Documents/ISE/ZomZorkProject/ZomZork/ZomZork.exe)
+set(__QT_DEPLOY_TARGET_ZomZork_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
