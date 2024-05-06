35be8fab8783883b1068dfbe608bae8a03d8d360
Elle0-0 <andreamichellevaz@gmail.com>
2024-05-06 17:04:16 +0100

    first implementation

--- ZomZork/mainwindow.cpp
+++ ZomZork/mainwindow.cpp
@@ -1,14 +1,44 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->stackedWidget->setCurrentIndex(0);
+    QPixmap pix(":/Zom-Zork.jpg");
+    ui->Bg_pic->setPixmap(pix.scaled(ui->Bg_pic->size(), Qt::IgnoreAspectRatio));
+
+    setFixedSize(1190, 651);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_1_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+void MainWindow::on_pushButton_3_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+void MainWindow::on_pushButton_4_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(4);
+}
+void MainWindow::on_pushButton_5_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+
--- ZomZork/mainwindow.h
+++ ZomZork/mainwindow.h
@@ -17,6 +17,14 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+
+    void on_pushButton_1_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_5_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
